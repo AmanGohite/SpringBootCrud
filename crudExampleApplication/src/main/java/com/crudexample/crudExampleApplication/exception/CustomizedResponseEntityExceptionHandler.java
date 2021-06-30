e209a5e4b673ce3a9b4eedef431c4c3533a517cf
AmanGohite <amangohite@gmail.com>
2021-06-30 12:51:58 +0530

    fixes

--- crudExampleApplication/src/main/java/com/crudexample/crudExampleApplication/exception/CustomizedResponseEntityExceptionHandler.java
+++ crudExampleApplication/src/main/java/com/crudexample/crudExampleApplication/exception/CustomizedResponseEntityExceptionHandler.java
@@ -15,19 +15,14 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestController
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
-//  @ExceptionHandler(Exception.class)
-//  public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
-//    ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(),
-//        request.getDescription(false));
-//    return new ResponseEntity(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
-//  }
-//
-//  @ExceptionHandler(StudentNotFoundException.class)
-//  public final ResponseEntity<Object> handleUserNotFoundException(StudentNotFoundException ex, WebRequest request) {
-//    ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(),
-//        request.getDescription(false));
-//    return new ResponseEntity(errorDetails, HttpStatus.NOT_FOUND);
-//  }
+  @ExceptionHandler(Exception.class)
+  public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
+    ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(),
+        request.getDescription(false));
+    return new ResponseEntity(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+
 
   @Override
   protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
